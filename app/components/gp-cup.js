7cf7c01836af7be1712c11fd4aa2312c6ad3da83
Cody Balos <cjbalos@gmail.com>
2017-06-13 00:53:57 -0700

    choose a course in progress

--- app/components/gp-cup.js
+++ app/components/gp-cup.js
@@ -1,17 +1,40 @@
 import Ember from 'ember';
 
 const {
-  Component
+  Component,
+  computed,
+  get,
+  set
 } = Ember;
 
 export default Component.extend({
-  completed: [
-    {id: 1, title: 'Luigi Raceway'}, {id: 2, title: 'Moo Moo Farms'}
-  ],
+  completedCourseIds: [1, 2],
+
+  completedCourses: computed('completedCourseIds.[]', function() {
+    let completedCourses = [];
+    let courses = get(this, 'cup.courses') || [];
+    get(this, 'completedCourseIds').forEach((id) => {
+      completedCourses.pushObject(courses.find((x) => x.id === id));
+    });
+    return completedCourses.map((course) => {
+      let n = 0;
+      completedCourses.forEach((course2) => {
+        return get(course, 'id') === get(course2, 'id') ? n++ : n;
+      });
+      set(course, 'count', n);
+      return course;
+    });
+  }),
+
+  showBackOfCard: false,
 
   actions: {
-    chooseARace() {
+    flipCard() {
+      this.toggleProperty('showBackOfCard');
+    },
 
+    chooseCourse(courseId) {
+      get(this, 'completedCourseIds').pushObject(courseId);
     }
   }
 });
